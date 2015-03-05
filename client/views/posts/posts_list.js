4011c86fe8664e4ef1b5a2ed8c188b9dd15d913b
Carol St.Louis <stlouis_c@yahoo.com>
2015-03-05 01:40:29 -0800

    tied collection into  template

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,28 @@
-var postsData = [
-	{
-		title:'Introducing Telescope',
-		author:'Sacha Greif',
-		url:'http://sachagreif.com/introducing-telescope'
-	},
-	{
-		title:'Meteor',
-		author:'Tom Coleman',
-		url:'http://meteor.com'
-	},
-	{
-		title:'The Meteor Book',
-		author:'Tom Coleman',
-		url:'http://themeteorbook.com'
-	}
-];
+//original hard-coding of data
+// var postsData = [
+// 	{
+// 		title:'Introducing Telescope',
+// 		author:'Sacha Greif',
+// 		url:'http://sachagreif.com/introducing-telescope'
+// 	},
+// 	{
+// 		title:'Meteor',
+// 		author:'Tom Coleman',
+// 		url:'http://meteor.com'
+// 	},
+// 	{
+// 		title:'The Meteor Book',
+// 		author:'Tom Coleman',
+// 		url:'http://themeteorbook.com'
+// 	}
+// ];
 // the following helper ties the above postsData to the posts template in posts_list.html, allowing the template to iterate through the above data
+// Template.postsList.helpers({
+// 	posts: postsData
+// });
+
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
