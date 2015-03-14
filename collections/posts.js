3b4817d094ceb5a79d1e775e5407b4cdbab8907a
Carol St.Louis <stlouis_c@yahoo.com>
2015-03-13 23:48:07 -0700

    removed meteor's default package insecure and added a temporary client-side permission to allow submission of posts by logged-in users

--- collections/posts.js
+++ collections/posts.js
@@ -3,3 +3,10 @@ Posts = new Meteor.Collection('posts');
 // Code inside folders that are not client/ or server/ will run in both contexts, but the collection does different things within the different envvironments
 //on the server, the collection has the job of talking to the Mongo database and writing any changes
 // on the client, the collection is a secure copy of a subset of the real, canonical collection; the client-side collection constantly and (mostly) transparently is kept up-to-date with that subset in real-time
+
+Posts.allow({
+	insert: function(userId, doc){
+		// only allow posting if user is logged in
+		return !! userId;
+	}
+});
