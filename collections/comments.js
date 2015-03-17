c0c2df4bbd3fb672c3a70b78d9d0a36b54d61858
Carol St.Louis <stlouis_c@yahoo.com>
2015-03-16 21:02:39 -0700

    added template submitting a new comment, event handler for form to pass submitted comment to the comments method and to handle returned errors & reset the form, and comments method to enforce comment having content, being submitted by a logged-in user, and being associated with a post

--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,23 @@
 Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+	comment: function(commentAttributes){
+		var user = Meteor.user();
+		var post = Posts.findOne(commentAttributes.postId);
+		if(!user){
+			throw new Meteor.Error(401, 'You need to login to make comments');
+		}
+		if(!commentAttributes.body){
+			throw new Meteor.Error(422, 'Please write some content;');
+		}
+		if(!post){
+			throw new Meteor.Error(422, 'Err.k...  Error....  Somehow you are submitting a comment, and the server is clueless re: which post is supposed to get the comment.  Things fall apart, the center can not hold, mere anarchy, blah, blah, blah.....');
+		}
+		comment = _.extend(commentAttributes, 'postId', 'body', {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date()
+		});
+		return Comments.insert(comment);
+	}
+});
