7abc87f172ff78077830ef040fe3f3fc3b538eef
Carol St.Louis <stlouis_c@yahoo.com>
2015-03-16 18:36:50 -0700

    added comments collection, fixture (fake data for development purposes), and publish/subscribe for comments

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,17 +1,75 @@
 if (Posts.find().count() === 0) {
-	Posts.insert({
+	var now = new Date().getTime();
+
+	// create two users
+	var tomId = Meteor.users.insert({
+		profile: { name: 'Tom Coleman' }
+	});
+	var tom = Meteor.users.findOne(tomId);
+	var sachaId = Meteor.users.insert({
+		profile: { name: 'Sacha Greif' }
+	});
+	var sacha = Meteor.users.findOne(sachaId);
+
+	// create posts
+	var telescopeId = Posts.insert({
 		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
+		userId: sacha._id,
+		author: sacha.profile.name,
+		url: 'http://sachagreif.com/introducing-telescope/',
+		submitted: now - 7 * 3600 * 1000
 	});
-	Posts.insert({
+
+	var MeteorId = Posts.insert({
 		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
+		userId: tom._id,
+		author: tom.profile.name,
+		url: 'http://meteor.com',
+		submitted: now - 4 * 3600 * 1000
 	});
-	Posts.insert({
+
+	var MeteorBookId = Posts.insert({
 		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
+		userId: tom._id,
+		author: tom.profile.name,
+		url: 'http://themeteorbook.com',
+		submitted: now - 12 * 3600 * 1000
 	});
+
+	// create comments
+	Comments.insert({
+		postId: telescopeId,
+		userId: tom._id,
+		author: tom.profile.name,
+		submitted: now - 5 * 3600 * 1000,
+		body: 'Interesting project, Sacha.  Can I get involved?'
+	});
+
+	Comments.insert({
+		postId: telescopeId,
+		userId: sacha._id,
+		author: sacha.profile.name,
+		submitted: now - 4.8 * 3600 *1000,
+		body: 'Indubitably!'
+	})
+
 }
+
+//deprecated after adding posts with comments
+// if (Posts.find().count() === 0) {
+// 	Posts.insert({
+// 		title: 'Introducing Telescope',
+// 		author: 'Sacha Greif',
+// 		url: 'http://sachagreif.com/introducing-telescope/'
+// 	});
+// 	Posts.insert({
+// 		title: 'Meteor',
+// 		author: 'Tom Coleman',
+// 		url: 'http://meteor.com'
+// 	});
+// 	Posts.insert({
+// 		title: 'The Meteor Book',
+// 		author: 'Tom Coleman',
+// 		url: 'http://themeteorbook.com'
+// 	});
+// }
