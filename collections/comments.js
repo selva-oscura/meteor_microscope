5f3b3c6f9a9da8ec54e06d720c78dcf334064515
Carol St.Louis <stlouis_c@yahoo.com>
2015-03-17 21:29:15 -0700

    added collection and method for creating notifications to original post author when comments on that post are made

--- collections/comments.js
+++ collections/comments.js
@@ -4,6 +4,8 @@ Meteor.methods({
 	comment: function(commentAttributes){
 		var user = Meteor.user();
 		var post = Posts.findOne(commentAttributes.postId);
+		
+		//checking that user is logged in, has written a comment, and is commenting on a post
 		if(!user){
 			throw new Meteor.Error(401, 'You need to login to make comments');
 		}
@@ -13,12 +15,26 @@ Meteor.methods({
 		if(!post){
 			throw new Meteor.Error(422, 'Erk...  Error....  Somehow you are submitting a comment, and the server is clueless re: which post is supposed to get the comment.  Things fall apart, the center can not hold, mere anarchy, blah, blah, blah.....');
 		}
+		
+		// create the comment object
 		comment = _.extend(commentAttributes, 'postId', 'body', {
 			userId: user._id,
 			author: user.username,
 			submitted: new Date()
 		});
+		
+		// update the posts record to indicate the addition of a comment
 		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
-		return Comments.insert(comment);
+		
+		// deprecated after adding notifications
+		// return Comments.insert(comment);
+
+		// insert the comment object into the Comments collection and capturing the _id of the new comment
+		comment._id = Comments.insert(comment);
+
+		// call the notification method to inform the post's original poster that there's been a comment
+		createCommentNotification(comment);
+
+		return comment._id;
 	}
 });
